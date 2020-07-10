0207b968eef66d3d534a1a6f2cae0dc18c808e19
Robson Piere <robsonpiere@outlook.com>
2020-07-10 16:52:45 -0300

    change http code to public

--- src/CiOutput.php
+++ src/CiOutput.php
@@ -4,11 +4,12 @@ namespace Sescmg\CiCore;
 
 class CiOutput extends \CI_Output
 {
-    protected const HTTP_OK = 200;
-    protected const HTTP_CREATED = 201;
+    public const HTTP_OK = 200;
+    public const HTTP_CREATED = 201;
 
-    protected const HTTP_BAD_REQUEST = 400;
-    protected const HTTP_UNAUTHORIZED = 401;
+    public const HTTP_BAD_REQUEST = 400;
+    public const HTTP_UNAUTHORIZED = 401;
+    public const HTTP_FORBIDDEN = 403;
 
 
     /**
